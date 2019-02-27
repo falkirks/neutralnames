2bbbe981bb3a4bec2869053817f4a80da745145f
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2019-02-27 20:08:21 +0000

    and that should be it

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask
-from datetime import datetime
+import random
+lines = open('names.txt').read().splitlines()
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    name = random.choice(lines)
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    """.format(time=the_time)
+    return name
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
