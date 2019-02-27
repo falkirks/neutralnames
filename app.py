d672d45e8ac739fd1715af842cfa4931587baec2
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2019-02-27 20:15:40 +0000

    CORS

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import random
 lines = open('names.txt').read().splitlines()
 app = Flask(__name__)
 
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/')
+@cross_origin()
 def homepage():
     name = random.choice(lines)
 
